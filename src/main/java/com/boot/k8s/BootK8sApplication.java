ab488a4d18f87fdfaeef1bde43c56db2501893bf
ratna1992 <ratnasrinivas1992@gmail.com>
2022-12-14 07:45:57 +0530

    first commit

---
+++ src/main/java/com/boot/k8s/BootK8sApplication.java
@@ -0,0 +1,13 @@
+package com.boot.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootK8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/k8s/BootK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
